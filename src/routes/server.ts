9688f491c03940082a95529253a1661362821a60
Wouter de Bruijn <git@electrogamez.nl>
2020-11-30 23:30:34 +0100

    :zap: Created custom queries to reduce size.

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -3,16 +3,26 @@
 */
 
 import { Router } from "express";
-import { Like } from "typeorm";
 import { Server } from "../entities/Server";
 import { User } from "../entities/User";
+import { RequestError } from "../express";
 import { checkToken } from "../middlewares/jwt";
 
 const router = Router();
 
 router.get("/", async (_req, res, next) => {
     try {
-        const servers = await Server.find();
+        const servers = await Server.createQueryBuilder()
+            .leftJoin("Server.owner", "owner")
+            .select([
+                "Server.id",
+                "Server.name",
+                "Server.description",
+                "owner.id",
+                "owner.name",
+            ])
+            .getMany();
+
         const guestView = servers.map((server) => server.dataAsGuest());
 
         res.json(guestView);
@@ -23,11 +33,19 @@ router.get("/", async (_req, res, next) => {
 
 router.get("/search/:name", async (req, res, next) => {
     try {
-        const servers = await Server.find({
-            where: {
-                name: Like(`%${req.params.name}%`),
-            },
-        });
+        const servers = await Server.createQueryBuilder()
+            .where("Server.name LIKE :query", {
+                query: `%${req.params.name}%`,
+            })
+            .leftJoin("Server.owner", "owner")
+            .select([
+                "Server.id",
+                "Server.name",
+                "Server.description",
+                "owner.id",
+                "owner.name",
+            ])
+            .getMany();
 
         res.json(servers.map((server) => server.dataAsGuest()));
     } catch (error) {
@@ -37,10 +55,36 @@ router.get("/search/:name", async (req, res, next) => {
 
 router.get("/id/:id", checkToken, async (req, res, next) => {
     try {
-        const server = (
-            await Server.findOneOrFail(req.params.id)
-        ).dataAsOwner();
-        res.json(server);
+        const server = await Server.createQueryBuilder()
+            .where("Server.id = :id", { id: req.params.id })
+            .leftJoin("Server.owner", "owner")
+            .select([
+                "Server.id",
+                "Server.name",
+                "Server.description",
+                "Server.ipAddress",
+                "Server.port",
+                "owner.id",
+                "owner.name",
+            ])
+            .getOne();
+
+        if (!server)
+            throw new RequestError(
+                "Server not found",
+                { id: req.params.id },
+                404
+            );
+
+        if (server.owner.id != res.locals.userId) {
+            throw new RequestError(
+                "You don't have permission for this server",
+                {},
+                403
+            );
+        }
+
+        res.json(server.dataAsOwner());
     } catch (error) {
         next(error);
     }
@@ -52,7 +96,7 @@ router.post("/", checkToken, async (req, res, next) => {
             req.body.ipAddress,
             req.body.port,
             req.body.password,
-            (await User.findOneOrFail(res.locals.userId)).dataAsOwner(),
+            await User.findOneOrFail(res.locals.userId),
             req.body.name,
             req.body.description
         ).save();
